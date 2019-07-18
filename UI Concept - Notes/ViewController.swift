a91ea91277c4f2d2f85aeaea543d8ade3542e1ec
Suprianto D <ersaka96@gmail.com>
2019-07-18 14:38:39 +0800

    Initial Commit

---
+++ UI Concept - Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI Concept - Notes
+//
+//  Created by Suprianto Djamalu on 18/07/19.
+//  Copyright © 2019 Suprianto Djamalu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
